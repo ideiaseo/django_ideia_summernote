f51eef103478e36a046efc042e54ab3de460910f
phillip <philliparente@gmail.com>
2016-08-04 17:01:55 -0300

    adding default config

--- django_ideia_summernote/widget.py
+++ django_ideia_summernote/widget.py
@@ -1,10 +1,26 @@
+import json
 from django.forms import Textarea
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
 
 __author__ = 'phillip'
 
-
+DEFAULT_EDITOR = {
+    'airMode': True,
+    'toolbar': [
+        # ['style', ['bold', 'italic', 'underline', 'clear']],
+        # ['font', ['strikethrough', 'superscript', 'subscript']],
+        # ['fontsize', ['fontsize']],
+        # ['color', ['color']],
+        # ['para', ['ul', 'ol', 'paragraph']],
+        ['height', ['height']]
+    ],
+    'popover': {
+        'air':[
+            ['para', ['ul', 'ol', 'paragraph']],
+        ]
+    }
+}
 
 class SummernoteWidget(Textarea):
 
@@ -13,6 +29,5 @@ class SummernoteWidget(Textarea):
 
 
     def render(self, name, value, attrs=None):
-
-
-        return mark_safe(render(None, 'ideia_summernote/default.html', {'name': name,}).content)
+        print json.dumps(DEFAULT_EDITOR)
+        return mark_safe(render(None, 'ideia_summernote/default.html', {'name': name, 'config': json.dumps(DEFAULT_EDITOR)}).content)
