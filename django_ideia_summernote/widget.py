fc495a3fc73c2ca72db6a63d5cd4a636d379ec82
phillip <philliparente@gmail.com>
2016-08-05 15:14:21 -0300

    basic editors config

--- django_ideia_summernote/widget.py
+++ django_ideia_summernote/widget.py
@@ -1,6 +1,6 @@
 import json
 from django.conf import settings
-from django.forms import Textarea
+from django import forms
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
 
@@ -8,16 +8,29 @@ __author__ = 'phillip'
 
 SUMMERNOTE_DEFAULT_CONFIG = {
 
+    'assets': {
+        'js': (
+            'https://code.jquery.com/jquery-2.2.4.min.js',
+            'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.1/summernote.min.js', ),
+
+        'css': {
+            'all': (
+                'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
+                'https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.1/summernote.css',
+            )
+        }
+    },
 
     'editors': {
         'default': {
             'airMode': False,
             'toolbar': [
-                # ['style', ['bold', 'italic', 'underline', 'clear']],
-                # ['font', ['strikethrough', 'superscript', 'subscript']],
-                # ['fontsize', ['fontsize']],
-                # ['color', ['color']],
-                # ['para', ['ul', 'ol', 'paragraph']],
+                ['style', ['bold', 'italic', 'underline', 'clear']],
+                ['font', ['strikethrough', 'superscript', 'subscript']],
+                ['fontsize', ['fontsize']],
+                ['color', ['color']],
+                ['para', ['ul', 'ol', 'paragraph']],
                 ['height', ['height']]
             ],
             'popover': {
@@ -26,40 +39,42 @@ SUMMERNOTE_DEFAULT_CONFIG = {
                 ]
             }
         },
-        'comment': {
-            'airMode': True,
-            'toolbar': [
-                # ['style', ['bold', 'italic', 'underline', 'clear']],
-                # ['font', ['strikethrough', 'superscript', 'subscript']],
-                # ['fontsize', ['fontsize']],
-                # ['color', ['color']],
-                # ['para', ['ul', 'ol', 'paragraph']],
-                ['height', ['height']]
-            ],
-            'popover': {
-                'air':[
-                    ['para', ['ul', 'ol', 'paragraph']],
-                ]
-            }
-        }
+
     }
-
-
 }
 
-class SummernoteWidget(Textarea):
+class SummernoteWidget(forms.Textarea):
+
+    class Media:
+        js = (
+            'http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.1/summernote.js',
+            # settings.STATIC_URL + 'ckeditor/ckeditor-init.js',
+        )
+
+    def _media(self):
+        print self.assets.get('js')
+        return forms.Media(css=self.assets.get('css'),
+                           js=self.assets.get('js'))
+    media = property(_media)
+
 
     def __init__(self, editor_conf=None, plugins=None, *args, **kwargs):
         self.editor_conf=editor_conf
         self.plugins = plugins
+
+        self.general_config = getattr(settings, 'SUMMERNOTE_CONFIG', SUMMERNOTE_DEFAULT_CONFIG)
+        self.editors = self.general_config.get('editors')
+
+        self.assets = self.general_config.get('assets', SUMMERNOTE_DEFAULT_CONFIG.get('assets'))
+
+
         super(SummernoteWidget, self).__init__(*args, **kwargs)
 
 
     def render(self, name, value, attrs=None):
-        general_config = getattr(settings, 'SUMMERNOTE_CONFIG', SUMMERNOTE_DEFAULT_CONFIG)
-        editors = general_config.get('editors')
-        editor_config = editors.get(self.editor_conf, editors.get('default'))
-        assets = general_config.get('assets', SUMMERNOTE_DEFAULT_CONFIG.get('assets'))
-        return mark_safe(render(None, 'ideia_summernote/default.html', {'name': name,
-                                                                        'config': json.dumps(editor_config)
+
+
+        return mark_safe(render(None, 'ideia_summernote/default.html', { 'self': self,
+                                                                        'name': name,
+                                                                        'config': json.dumps(self.editor_conf)
                                                                         }).content)
