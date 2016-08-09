fa4b9ca205b11d9dafec0c208563fdf8c1a02ad6
phillip <philliparente@gmail.com>
2016-08-09 17:33:07 -0300

    fixing editor conf

--- ideia_summernote/fields.py
+++ ideia_summernote/fields.py
@@ -11,6 +11,9 @@ class SummernoteFormField(CharField):
         kwargs.update({'widget': SummernoteWidget(editor_conf=editor_conf, load_init=load_init, async=async)})
         super(SummernoteFormField, self).__init__(*args, **kwargs)
 
+    def get_editor(self):
+        return self.widget.get_editor()
+
 
 class SummernoteField(models.TextField):
 
