d11d3fca10403b4d62ed5c7810d55459ab61309b
phillip <philliparente@gmail.com>
2016-08-09 16:02:33 -0300

    adding field to demo app

--- demo/admin.py
+++ demo/admin.py
@@ -1,15 +1,4 @@
 from django.contrib import admin
-from django import forms
 from demo.models import Book
-from ideia_summernote.widget import SummernoteWidget
 
-
-class BookAdminForm(forms.ModelForm):
-    class Meta:
-        widgets = {'text': SummernoteWidget(editor_conf='another')}
-        excludes = ()
-
-class BookAdmin(admin.ModelAdmin):
-    form = BookAdminForm
-
-admin.site.register(Book, BookAdmin)
+admin.site.register(Book)
--- demo/models.py
+++ demo/models.py
@@ -3,9 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+from ideia_summernote.fields import SummernoteField
 
 
 class Book(models.Model):
 
     title = models.CharField(null=False, blank=False, max_length=100)
-    text = models.TextField(null=False, blank=False)
+    text = SummernoteField(null=False, blank=False)
