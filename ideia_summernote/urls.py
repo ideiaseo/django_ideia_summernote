7a8c3efee20ddb457a2cc23618f3afa10ab5e6c0
phillip <philliparente@gmail.com>
2016-08-19 13:38:52 -0300

    fixing localimports

--- ideia_summernote/urls.py
+++ ideia_summernote/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from ..ideia_summernote.views import Upload
+from ideia_summernote.views import Upload
 urlpatterns = [
     url(r'^upload', Upload.as_view(), name='upload'),
 
